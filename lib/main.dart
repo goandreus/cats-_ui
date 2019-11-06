4781de8c41a10b7c7311bc2603fc09ce2959c1c8
goandreus <andreshtml5@gmail.com>
2019-11-05 20:13:07 -0500

    utils

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,19 @@ import 'package:catbox/ui/cat_list.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  runApp(new CatBoxApp());
+  runApp( CatBoxApp());
 }
 
 class CatBoxApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      theme: new ThemeData(
+    return  MaterialApp(
+      theme:  ThemeData(
         primarySwatch: Colors.blue,
         accentColor: Colors.pinkAccent,
         fontFamily: 'Ubuntu'
       ),
-      home: new CatList(),
+      home:  CatList(),
     );
   }
 }
